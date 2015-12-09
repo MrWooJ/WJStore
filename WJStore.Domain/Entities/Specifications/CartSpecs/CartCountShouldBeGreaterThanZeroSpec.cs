698f80a964c65d423505cc6c9237d68f9fa9bb2c
Alireza Arabi <hurricanc@gmail.com>
2015-12-08 22:54:06 -0800

    Implement CartSpecs to Control Specification

---
+++ WJStore.Domain/Entities/Specifications/CartSpecs/CartCountShouldBeGreaterThanZeroSpec.cs
@@ -0,0 +1,12 @@
+﻿using WJStore.Domain.Interfaces.Validation;
+
+namespace WJStore.Domain.Entities.Specifications.CartSpecs
+{
+    public class CartCountShouldBeGreaterThanZeroSpec : ISpecification<Cart>
+    {
+        public bool IsSatisfiedBy(Cart cart)
+        {
+            return cart.Count > 0;
+        }
+    }
+}
