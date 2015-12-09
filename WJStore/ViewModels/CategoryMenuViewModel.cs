db4531b7927911471a9c2077184a59b76ed8f673
Alireza Arabi <hurricanc@gmail.com>
2015-12-09 01:26:58 -0800

    Implement ViewModels by Mapping Models to Views

---
+++ WJStore/ViewModels/CategoryMenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WJStore.Domain.Entities;
+
+namespace WJStore.ViewModels
+{
+    public class CategoryMenuViewModel
+    {
+        public string Name { get; set; }
+
+        public static CategoryMenuViewModel ToViewModel(Category category)
+        {
+            return Mapper.Map<CategoryMenuViewModel>(category);
+        }
+    }
+}
---
+++ WJStore/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WJStore.Domain.Entities;
+
+namespace WJStore.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
