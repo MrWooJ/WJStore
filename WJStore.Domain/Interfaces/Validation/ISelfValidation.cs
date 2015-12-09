7a8c4bf5e40db605ce55294d5e9f2e66b8302e27
Alireza Arabi <hurricanc@gmail.com>
2015-12-08 23:05:29 -0800

    Implement Value Validation Process Interface

---
+++ WJStore.Domain/Interfaces/Validation/ISelfValidation.cs
@@ -0,0 +1,10 @@
+﻿using WJStore.Domain.Validation;
+
+namespace WJStore.Domain.Interfaces.Validation
+{
+    public interface ISelfValidation
+    {
+        ValidationResult ValidationResult { get; }
+        bool IsValid { get; }
+    }
+}
---
+++ WJStore.Domain/Interfaces/Validation/IValidation.cs
@@ -0,0 +1,9 @@
+﻿using WJStore.Domain.Validation;
+
+namespace WJStore.Domain.Interfaces.Validation
+{
+    public interface IValidation<in TEntity>
+    {
+        ValidationResult Valid(TEntity entity);
+    }
+}
