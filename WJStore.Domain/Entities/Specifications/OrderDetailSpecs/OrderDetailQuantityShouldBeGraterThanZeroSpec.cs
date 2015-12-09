6fa5d862e26851200aa8404d8089ab9be8dee34c
Alireza Arabi <hurricanc@gmail.com>
2015-12-08 22:55:12 -0800

    Implement OrderDetailsSpecs to Control Specification

---
+++ WJStore.Domain/Entities/Specifications/OrderDetailSpecs/OrderDetailQuantityShouldBeGraterThanZeroSpec.cs
@@ -0,0 +1,12 @@
+﻿using WJStore.Domain.Interfaces.Validation;
+
+namespace WJStore.Domain.Entities.Specifications.OrderDetailSpecs
+{
+    public class OrderDetailQuantityShouldBeGraterThanZeroSpec : ISpecification<OrderDetail>
+    {
+        public bool IsSatisfiedBy(OrderDetail orderDetail)
+        {
+            return orderDetail.Quantity > 0;
+        }
+    }
+}
---
+++ WJStore.Domain/Entities/Specifications/OrderDetailSpecs/OrderDetailUnityPriceShouldBeGraterThanZeroSpec.cs
@@ -0,0 +1,12 @@
+﻿using WJStore.Domain.Interfaces.Validation;
+
+namespace WJStore.Domain.Entities.Specifications.OrderDetailSpecs
+{
+    public class OrderDetailUnityPriceShouldBeGraterThanZeroSpec : ISpecification<OrderDetail>
+    {
+        public bool IsSatisfiedBy(OrderDetail orderDetail)
+        {
+            return orderDetail.UnitPrice > 0;
+        }
+    }
+}
