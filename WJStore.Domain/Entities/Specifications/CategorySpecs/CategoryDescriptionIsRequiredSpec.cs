0b256e320397e564762997592685b72a8937ea6c
Alireza Arabi <hurricanc@gmail.com>
2015-12-08 22:54:39 -0800

    Implement CategorySpecs to Control Specification

---
+++ WJStore.Domain/Entities/Specifications/CategorySpecs/CategoryDescriptionIsRequiredSpec.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WJStore.Domain.Interfaces.Validation;
+
+namespace WJStore.Domain.Entities.Specifications.CategorySpecs
+{
+    public class CategoryDescriptionIsRequiredSpec : ISpecification<Category>
+    {
+        public bool IsSatisfiedBy(Category Category)
+        {
+            return !String.IsNullOrEmpty(Category.Description) && !String.IsNullOrWhiteSpace(Category.Description);
+        }
+    }
+}
---
+++ WJStore.Domain/Entities/Specifications/CategorySpecs/CategoryNameIsRequiredSpec.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WJStore.Domain.Interfaces.Validation;
+
+namespace WJStore.Domain.Entities.Specifications.CategorySpecs
+{
+    public class CategoryNameIsRequiredSpec : ISpecification<Category>
+    {
+        public bool IsSatisfiedBy(Category Category)
+        {
+            return !String.IsNullOrEmpty(Category.Name) && !String.IsNullOrWhiteSpace(Category.Name);
+        }
+    }
+}
