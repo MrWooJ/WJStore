933c195ca4f11e0ed5162318df1190fb87c8c4e2
Alireza Arabi <hurricanc@gmail.com>
2015-12-09 01:24:16 -0800

    Handle Mapping Between Entity Models and ViewModels

---
+++ WJStore/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WJStore.Domain.Entities;
+using WJStore.ViewModels;
+
+namespace WJStore.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<CategoryMenuViewModel, Category>();
+        }
+    }
+}
---
+++ WJStore/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WJStore.Domain.Entities;
+using WJStore.ViewModels;
+
+namespace WJStore.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Category, CategoryMenuViewModel>();
+        }
+    }
+}
