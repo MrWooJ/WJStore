ee13bb9fe5cdfc925830c01b17df3ee79b4e0092
Alireza Arabi <hurricanc@gmail.com>
2015-12-31 12:49:48 -0800

    Add Main Basic Selenium Test Functions

---
+++ WJStore.Test/WJStore.Test/SeleniumTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace WJStore.Test
+{
+    [TestClass]
+    public abstract class SeleniumTest {
+
+        const int iisPort = 26641;
+        private string _applicationName;
+        private Process _iisProcess;
+ 
+        protected SeleniumTest(string applicationName) {
+            _applicationName = applicationName;
+        }
+ 
+ 
+        public FirefoxDriver FirefoxDriver { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize() {
+            // Start IISExpress
+            StartIIS();
+ 
+            // Start Selenium drivers
+            this.FirefoxDriver = new FirefoxDriver();
+        }
+ 
+ 
+        [TestCleanup]
+        public void TestCleanup() {
+            // Ensure IISExpress is stopped
+            if (_iisProcess.HasExited == false) {
+                _iisProcess.Kill();
+            }
+ 
+            // Stop all Selenium drivers
+            this.FirefoxDriver.Quit();
+        }
+ 
+ 
+ 
+        private void StartIIS() {
+            var applicationPath = GetApplicationPath(_applicationName);
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+ 
+            _iisProcess = new Process();
+            _iisProcess.StartInfo.FileName = programFiles + "\\IIS Express\\iisexpress.exe";
+            _iisProcess.StartInfo.Arguments = string.Format("/path:{0} /port:{1}", applicationPath, iisPort);
+            _iisProcess.Start();
+        }
+ 
+ 
+        protected virtual string GetApplicationPath(string applicationName) {
+            var solutionFolder = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)));
+            return Path.Combine(solutionFolder, applicationName);
+        }
+ 
+ 
+        public string GetAbsoluteUrl(string relativeUrl) {
+            if (!relativeUrl.StartsWith("/")) {
+                relativeUrl = "/" + relativeUrl;
+            }
+            return String.Format("http://localhost:{0}{1}", iisPort, relativeUrl);
+        }
+ 
+ 
+    }
+}
---
+++ WJStore.Test/WJStore.Test/ShoppingTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace WJStore.Test.Controllers
+{
+    [TestClass]
+    public class ShoppingTest : SeleniumTest
+    {
+
+        public ShoppingTest() : base("WJStore") { }
+
+
+        [TestMethod]
+        public void IndexFirefoxTest()
+        {
+            // Act
+            this.FirefoxDriver.Navigate().GoToUrl(this.GetAbsoluteUrl(""));
+        }
+    }
+}
